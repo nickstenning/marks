d51a500c6b49e3eaf7bdce9be2cb64fcf9bdd830
Nick Stenning <nick@whiteink.com>
2015-02-18 20:21:46 +0000

    Add namespace check to svg.createElement tests

--- test/svg_test.js
+++ test/svg_test.js
@@ -6,5 +6,10 @@ describe('svg', function () {
             var elem = svg.createElement('g');
             assert.equals(elem.tagName, 'g');
         });
+
+        it('creates an element with the SVG namespace URI', function () {
+            var elem = svg.createElement('g');
+            assert.equals(elem.namespaceURI, 'http://www.w3.org/2000/svg');
+        });
     });
 });
